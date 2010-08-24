fc6ddf035a5c40f311e942a88ff55b8075287e02
Michał Górny <gentoo@mgorny.alt.pl>
2010-08-25 00:16:46 +0200

    Bump PV to 0.5.2.

--- smart-live-rebuild.py
+++ smart-live-rebuild.py
@@ -4,7 +4,7 @@
 # (C) 2010 Michał Górny <gentoo@mgorny.alt.pl>
 # Released under the terms of the 3-clause BSD license or the GPL-2 license.
 
-PV = '0.5.1'
+PV = '0.5.2'
 
 import bz2, os, pickle, re, shutil, signal, subprocess, sys, tempfile, time
 import hashlib # CVS
