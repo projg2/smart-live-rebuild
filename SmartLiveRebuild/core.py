2f5fa1d1055244c87b82597e565955e21b13d93d
Michał Górny <gentoo@mgorny.alt.pl>
2010-09-02 00:01:07 +0200

    Move configuration file parsing to SmartLiveRebuild.core.
    Now, optparse is no longer prioritized. Instead, SmartLiveRebuild.core
    introduces a Config class, encapsulating the default settings,
    configuration file parsing and option passing helpers.
    
    The smart-live-rebuild script uses optparse to parse the command-line
    options and passes them to the Config instance (twice). It also engages
    the configfile parsing and passes the final configuration (in a
    values-like form) from the Config instance to the SmartLiveRebuild()
    func.

--- SmartLiveRebuild/core.py
+++ SmartLiveRebuild/core.py
@@ -1,8 +1,115 @@
 import bz2, os.path, pickle, signal, subprocess, sys, tempfile, time
 import portage
 
+try:
+	from configparser import ConfigParser, NoOptionError
+except ImportError: # py2
+	from ConfigParser import ConfigParser, NoOptionError
+
 from SmartLiveRebuild.output import out
 from SmartLiveRebuild.vcs import NonLiveEbuild
+
+class Config(ConfigParser):
+	def __init__(self):
+		self._real_defaults = {
+			'color': 'True',
+			'config_file': '/etc/portage/smart-live-rebuild.conf',
+			'erraneous_merge': 'True',
+			'jobs': '1',
+			'local_rev': 'False',
+			'network': 'True',
+			'offline': 'True',
+			'pretend': 'False',
+			'profile': 'smart-live-rebuild',
+			'quickpkg': 'False',
+			'setuid': str('userpriv' in portage.settings.features),
+			'type': '',
+			'unprivileged_user': 'False'
+		}
+
+		self._current_section = 'DEFAULT'
+		ConfigParser.__init__(self, self._real_defaults)
+
+	def apply_optparse(self, values):
+		for k in self.defaults():
+			try:
+				v = getattr(values, k)
+				if v is None:
+					raise ValueError
+			except (AttributeError, ValueError):
+				pass
+			else:
+				self.set(self._current_section, k, str(v))
+
+	def parse_configfiles(self):
+		cfl = [self.get(self._current_section, 'config_file')]
+		sect = self.get(self._current_section, 'profile')
+
+		try:
+			while cfl[-1] != '' and self.read(os.path.expanduser(cfl[-1])):
+				# config file chaining support
+				try:
+					cf = self.get(sect, 'config_file')
+				except NoOptionError:
+					break
+				else:
+					if cf not in cfl:
+						cfl.append(cfl)
+					else:
+						break
+		except Exception as e:
+			out.err('Error while parsing configuration file:')
+			out.err('%s: %s' % (e.__class__.__name__, e))
+
+		if not self.has_section(sect):
+			self.add_section(sect)
+		self._current_section = sect
+
+	def get_options(self):
+		# values can be accessible both through keys (like a dict)
+		# and through attributes (like optparse's values)
+		class ConfigValues(object):
+			def __getitem__(self, k):
+				return getattr(self, k)
+
+			def __setitem__(self, k, v):
+				return setattr(self, k, v)
+
+			def __str__(self):
+				out = {}
+				for k in dir(self):
+					if not k.startswith('_'):
+						out[k] = getattr(self, k)
+
+				return str(out)
+
+		# use the _real_defaults as type-hint
+		val = ConfigValues()
+
+		for k, v in self.items(self._current_section):
+			if k not in self._real_defaults:
+				val[k] = v
+			elif self._real_defaults[k] in ('True', 'False'): # bool
+				try:
+					val[k] = self.getboolean(self._current_section, k)
+				except ValueError:
+					out.err('Incorrect boolean value: %s=%s' % (k, v))
+					val[k] = (self._real_defaults[k] == 'True')
+			elif k == 'jobs': # int
+				try:
+					val[k] = int(v)
+				except ValueError:
+					out.err('Incorrect int value: %s=%s' % (k, v))
+					val[k] = int(self._real_defaults[k])
+			elif k == 'type':
+				if v != '':
+					val[k] = v.split(',')
+				else:
+					val[k] = None
+			else:
+				val[k] = v
+
+		return val
 
 def SmartLiveRebuild(opts, args):
 	if not opts.color:
@@ -238,4 +345,3 @@ user account, please pass the --unprivileged-user option.
 			os.kill(childpid, signal.SIGTERM)
 
 	return 0
-
