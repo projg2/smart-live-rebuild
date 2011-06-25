829022bdd3f6adf50ed20c7fc687b8b037cb22a6
Michał Górny <mgorny@gentoo.org>
2011-06-25 13:02:12 +0200

    git: reuse git-2 eclass code.

--- smartliverebuild/vcs/git.py
+++ smartliverebuild/vcs/git.py
@@ -2,26 +2,11 @@
 # (c) 2011 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 3-clause BSD license or the GPL-2 license.
 
-from smartliverebuild.vcs import VCSSupport, NonLiveEbuild
+from smartliverebuild.vcs.git_2 import Git2Support
 
-class GitSupport(VCSSupport):
-	reqenv = ['EGIT_BRANCH', 'EGIT_REPO_URI', 'EGIT_VERSION']
-	optenv = ['EGIT_COMMIT']
-
-	def __init__(self, *args):
-		VCSSupport.__init__(self, *args)
-		if self.env['EGIT_COMMIT'] and self.env['EGIT_COMMIT'] != self.env['EGIT_BRANCH']:
-			raise NonLiveEbuild('EGIT_COMMIT set, package is not really a live one')
-
-	def __str__(self):
-		return self.env['EGIT_REPO_URI']
-
-	def parseoutput(self, out):
-		return out.split()[0]
-
-	def getsavedrev(self):
-		return self.env['EGIT_VERSION']
-
-	def getupdatecmd(self):
-		return 'git ls-remote --heads %s %s' % (
-				self.env['EGIT_REPO_URI'], self.env['EGIT_BRANCH'])
+class GitSupport(Git2Support):
+	def __init__(self, *args, **kwargs):
+		Git2Support.__init__(self, *args, **kwargs)
+		if len(self.repo_uris) != 1:
+			raise ValueError('EGIT_REPO_URI has to contain a single URI (not "%s")' \
+					% self.env['EGIT_REPO_URI'])
