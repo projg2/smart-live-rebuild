db8f1f8c8b952e616da4a53c57c5e344a4f241d6
Michał Górny <mgorny@gentoo.org>
2011-08-31 08:28:42 +0200

    Mercurial: detect non-live packages.

--- smartliverebuild/vcs/mercurial.py
+++ smartliverebuild/vcs/mercurial.py
@@ -2,12 +2,17 @@
 # (c) 2011 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
-from . import RemoteVCSSupport
+from . import RemoteVCSSupport, NonLiveEbuild
 
 class MercurialSupport(RemoteVCSSupport):
 	reqenv = ['EHG_REPO_URI', 'EHG_REVISION', 'HG_REV_ID']
 
 	trustopt = ['--config', 'trusted.users=portage'] # XXX: pm.config.userpriv_uid
+
+	def __init__(self, *args, **kwargs):
+		RemoteVCSSupport.__init__(self, *args, **kwargs)
+		if self.revcmp(self.savedrev, self.env['EHG_REVISION']):
+			raise NonLiveEbuild('EHG_REVISION set to a revision, package is not really a live one')
 
 	def __str__(self):
 		return self.env['EHG_REPO_URI']
@@ -18,7 +23,8 @@ class MercurialSupport(RemoteVCSSupport):
 
 	@staticmethod
 	def revcmp(oldrev, newrev):
-		return newrev.startswith(oldrev)
+		# assume either can be longer
+		return newrev.startswith(oldrev) or oldrev.startswith(newrev)
 
 	@property
 	def updatecmd(self):
