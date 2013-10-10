1b0af74c8c78d772b8e023a9c52bab77b38805c6
Michał Górny <mgorny@gentoo.org>
2013-10-10 08:19:03 +0200

    Revert "Remove redundant git-2 support."
    Packages installed before git-r3 was introduced do not inherit it
    implicitly yet, and therefore don't get rebuilt at all.

---
+++ smartliverebuild/vcs/git_2.py
@@ -0,0 +1,8 @@
+#	vim:fileencoding=utf-8
+# (c) 2011 Michał Górny <mgorny@gentoo.org>
+# Released under the terms of the 2-clause BSD license.
+
+from .git_r3 import GitR3Support
+
+class Git2Support(GitR3Support):
+	pass
