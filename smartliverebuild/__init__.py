bb9e6f97aa9c7da230de6b0238f1d11a2ff12d31
Michał Górny <mgorny@gentoo.org>
2014-12-03 18:43:33 +0100

    Bump PV to 1.3.4.1

--- smartliverebuild/__init__.py
+++ smartliverebuild/__init__.py
@@ -1,5 +1,5 @@
 #	vim:fileencoding=utf-8
-# (c) 2010-2012 Michał Górny <mgorny@gentoo.org>
+# (c) 2010-2014 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
-PV = '1.3.4'
+PV = '1.3.4.1'
