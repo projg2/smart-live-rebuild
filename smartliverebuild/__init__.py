5c31aa2fc320cdc32fea104e53f967d8ab20ec80
Michał Górny <mgorny@gentoo.org>
2017-08-24 23:42:41 +0200

    Bump to 1.3.5

--- smartliverebuild/__init__.py
+++ smartliverebuild/__init__.py
@@ -1,5 +1,5 @@
 #	vim:fileencoding=utf-8:noet
-# (c) 2010-2014 Michał Górny <mgorny@gentoo.org>
+# (c) 2010-2017 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
-PV = '1.3.4.1'
+PV = '1.3.5'
