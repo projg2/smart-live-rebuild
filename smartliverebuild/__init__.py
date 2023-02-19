72f4aca629a11de561b830a1600012ef7eaf27b8
Michał Górny <mgorny@gentoo.org>
2023-02-19 20:53:24 +0100

    Bump to 1.4.1
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- smartliverebuild/__init__.py
+++ smartliverebuild/__init__.py
@@ -1,7 +1,7 @@
 # 	vim:fileencoding=utf-8:noet
-# (c) 2010-2022 Michał Górny <mgorny@gentoo.org>
+# (c) 2010-2023 Michał Górny <mgorny@gentoo.org>
 # Released under the terms of the 2-clause BSD license.
 
 """A tool to rebuild live Gentoo packages on VCS changes"""
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
